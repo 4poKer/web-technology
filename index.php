b59749b8f068d4c0ebf95e7cf3adbb3e3ca8b5c9
4poKer <arseniy59@yandex.ru>
2015-04-09 15:44:18 +0500

    lab4

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="/main.css">
+    </head>
+    <body>
+        <div id="SCSI">
+            <div id="title">
+                <div id="chertichka22">
+                    <div id="ch22">
+                    </div>
+                    <div id="l22">
+                    </div>
+                    <div id="l222">
+                    </div>                   
+                </div>
+                <span id="AD">SCSI Adapter Setup</span>
+            </div>
+            <div id="wh">
+                <div id="left">
+                    <span id="t">SCSI A<span class="f">d</span>apters installed on COMP2:</span>
+                    <div id="tb">
+                        <div id="p">Future Domain 16xx</div>
+                    </div>
+                </div>
+                <div id="right">
+                    <div id="btn1">
+                        <div class="bord">
+                            <span id="close"> Close</span>
+                        </div>
+                    </div>
+                    
+                    <div id="btn2">
+                        <div class="bord">
+                            <span id="add"> <span class="f">A</span>dd...</span>
+                        </div>
+                    </div>
+                    
+                     <div id="btn3">
+                        <div class="bord">
+                            <span id="remove"> <span class="f">R</span>emove</span>
+                        </div>
+                    </div>
+                    
+                     <div id="btn4">
+                        <div class="bord">
+                            <span id="help"> <span class="f">H</span>elp</span>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div style=" width:100%; height:1px; clear:both;"></div>
+            </div>
+        </div>
+
+
+
+        <table id="WNTS" cellspacing="0">
+            <tr>
+                <td id="chertichka">
+                    <div id="ch">
+                    </div>
+                    <div id="l2">
+                    </div>
+                    <div id="l">
+                    </div>
+                </td>
+                <td id="setup">
+                    Windows NT Setup
+                </td>
+                <td id="treugolnik">
+                    <div id="br">
+                        <div id="tr">
+                        </div>
+                    </div>
+                </td>
+            </tr>
+            <tr id="head" id="menu"><td colspan="3">
+                    <span id="Options"><span class="f">O</span>ptions</span>
+                    <span id="Help"><span class="f">H</span>elp</span>
+                </td></tr>
+            <tr><td colspan="3">
+                    <div id="pr">
+                        <p>Display:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GD5426/28 v1.41 800x600, 256 colors 60Hz</p>
+                        <p>Mouse:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Logitech Serial Mouse</p>
+                        <p>Keybord:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;XT, AT or Enhanced Keyboard (83-102 key)</p>
+                    </div>
+                </td></tr>
+        </table
+
+    </body>
+</html>
